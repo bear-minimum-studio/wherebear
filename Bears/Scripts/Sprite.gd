fdaa82b029d181082ab0d1cf4a4db0c47737fd09
Alexandre BOBET <bobet.alexandre@gmail.com>
2022-01-22 00:48:35 +0100

    Add generic bear

---
+++ Bears/Scripts/Sprite.gd
@@ -0,0 +1,11 @@
+extends Sprite
+
+const ORIENTATION_SWAP_THRESHOLD := 5.0
+
+func _ready():
+	pass
+
+func set_orientation(horizontal_speed: float):
+	if(abs(horizontal_speed) > ORIENTATION_SWAP_THRESHOLD):
+		var orientation = -sign(horizontal_speed) * abs(self.scale.x)
+		self.scale.x = orientation
