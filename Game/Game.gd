bff635bba31a397e52a5299422b5471fa7672e55
Alexandre BOBET <bobet.alexandre@gmail.com>
2022-01-22 19:00:56 +0100

    Removing warning

--- Game/Game.gd
+++ Game/Game.gd
@@ -15,7 +15,7 @@ func _ready():
 	var spawn_zone_top_left_absolute_coordinates_y = (spawn_zone_area.global_position.y - square_extents.y)
 
 	Logger.info("Spawning bears")
-	for i in range(NUMBER_OF_BEARS):
+	for _i in range(NUMBER_OF_BEARS):
 		var x = rand_range(0, square_extents.x * 2)
 		var y = rand_range(0, square_extents.y * 2)
 
