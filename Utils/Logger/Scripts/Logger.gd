645d741944f9a91fd66e7fbe0eac8d8fb010f67b
Alexandre BOBET <bobet.alexandre@gmail.com>
2022-01-22 21:00:50 +0100

    Fix Logger bug where it couldn't display objects

--- Utils/Logger/Scripts/Logger.gd
+++ Utils/Logger/Scripts/Logger.gd
@@ -3,15 +3,15 @@ const LOGGER_LEVEL := 3
 
 static func info(message) -> void:
 	if(LOGGER_LEVEL > 2):
-		print('INFO: ' + message)
+		print('INFO: ', message)
 
 static func debug(message) -> void:
 	if(LOGGER_LEVEL > 1):
-		print('DEBUG: ' + message)
+		print('DEBUG: ', message)
 
 static func warn(message) -> void:
 	if(LOGGER_LEVEL > 0):
-		print('WRAN: ' + message)
+		print('WRAN: ', message)
 
 static func err(message) -> void:
-	printerr('ERROR: ' + message)
+	printerr('ERROR: ', message)
