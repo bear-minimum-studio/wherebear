b393ace85ccb6dae9678780a8669d3a6e5c21742
Pierre Poupin <pierrep@bam.tech>
2022-01-23 19:21:29 +0100

    add log

--- Game/Scripts/Game.gd
+++ Game/Scripts/Game.gd
@@ -4,9 +4,12 @@ func _ready():
 	Events.connect("round_ended", self, "_on_round_ended")
 
 func _on_round_ended(score):
+	Logger.info("Game ended!")
 	$UICanvas/ScoreDisplay.display_score(score)
-	
+
 	var timer = get_tree().create_timer(5.0)
 	yield(timer, "timeout")
 	
+	Logger.info("Reloading scene...")
 	get_tree().reload_current_scene()
+	Logger.info("Reloaded!")
