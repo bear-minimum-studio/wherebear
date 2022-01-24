3ba17dbbe520027e90ba098565e567b887a6aa8a
Pierre Poupin <pierrep@bam.tech>
2022-01-24 23:12:14 +0100

    add logic to end intro

--- UI/Intro/Intro.gd
+++ UI/Intro/Intro.gd
@@ -5,16 +5,35 @@ onready var dialog_iterator = $DialogIterator
 
 const STORY_2_SCREEN_INDEX = 3
 
+var _final_animation_complete = false
+var _dialog_iterator_ended = false
+
 func _ready():
 	dialog_iterator.start()
 	dialog_iterator.connect("screen_end", self, "_next_story")
 
+func _try_to_finish_intro():
+	if _final_animation_complete and _dialog_iterator_ended:
+		Logger.info("Intro is finished!")
+		pass
+
 func _input(event):
 	if (event is InputEventJoypadButton or event is InputEventKey) and event.pressed:
-		Logger.info("Going to next cutscene")
+		Logger.debug("Going to next dialog")
 		dialog_iterator.next()
+		_try_to_finish_intro()
+
 
 func _next_story(current_screen):
 	if current_screen == STORY_2_SCREEN_INDEX:
 		var state_machine = anim_tree["parameters/playback"]
 		state_machine.travel("Cutscene 2")
+
+func _on_AnimationPlayer_animation_finished():
+	Logger.debug("Last animation complete")
+	_final_animation_complete = true
+	_try_to_finish_intro()
+
+func _on_DialogIterator_end():
+	_dialog_iterator_ended = true
+	_try_to_finish_intro()
