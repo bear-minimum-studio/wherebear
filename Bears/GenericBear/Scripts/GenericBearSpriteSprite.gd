df42e390ad847d6d630d12afe09321961d9d081c
Pierre Poupin <pierrep@bam.tech>
2022-02-01 00:06:24 +0100

    feature: change bear skin colors randomly

--- Bears/GenericBear/Scripts/GenericBearSpriteSprite.gd
+++ Bears/GenericBear/Scripts/GenericBearSpriteSprite.gd
@@ -3,6 +3,11 @@ extends Sprite
 
 const ORIENTATION_SWAP_THRESHOLD := 5.0
 
+var SKIN_COLOR_VARIANTS = [
+	{ 1: Color("#000000") , 2: Color("#000000"), 3: Color("#000000") }, # default colors - mid brown
+	{ 1: Color("#502a10") , 2: Color("#7e4216"), 3: Color("#372110") }, # dark brown
+	{ 1: Color("#767370") , 2: Color("#524a45"), 3: Color("#3a3836") }, # grey
+]
 
 func set_orientation(speed: Vector2):
 	if(abs(speed.x) > ORIENTATION_SWAP_THRESHOLD):
@@ -13,6 +18,11 @@ func _ready():
 	self.material = self.material.duplicate()
 	self.material.set_shader_param("u_replacement_color", _random_color())
 	self.material.set_shader_param("u_replacement_color_2", _random_color())
+
+	var random_color_variant = randi() % SKIN_COLOR_VARIANTS.size()
+	self.material.set_shader_param("u_skin_color_replacement_1", SKIN_COLOR_VARIANTS[random_color_variant][1])
+	self.material.set_shader_param("u_skin_color_replacement_2", SKIN_COLOR_VARIANTS[random_color_variant][2])
+	self.material.set_shader_param("u_skin_color_replacement_3", SKIN_COLOR_VARIANTS[random_color_variant][3])
 
 func _random_color():
 	randomize()
