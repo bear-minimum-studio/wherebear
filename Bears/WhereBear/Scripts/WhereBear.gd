87284cf9c245e1eac0b3f708d738476f41d03904
Firliminat <bobet.alexandre@gmail.com>
2022-04-30 18:45:51 +0200

    Fix biting prio bug

--- Bears/WhereBear/Scripts/WhereBear.gd
+++ Bears/WhereBear/Scripts/WhereBear.gd
@@ -20,10 +20,40 @@ func _bite() -> void:
 		return
 	
 	var overlapping_areas = hit_box.get_overlapping_areas()
+	if overlapping_areas == null || overlapping_areas.size() < 1:
+		return
+	overlapping_areas.sort_custom(self, "_bite_compare")
 	for overlapping_area in overlapping_areas:
 		if(overlapping_area is HurtBox && overlapping_area.owner is NonPlayableBear):
 			overlapping_area.owner.contaminate()
 			break
+
+func _bite_compare(a : Area2D, b : Area2D) -> bool:
+	var keep_a := true
+	var keep_b := false
+	
+	# If one is not a HurtBox keep the other
+	# If both are not keep a
+	if !(b is HurtBox):
+		return keep_a
+	if !(a is HurtBox):
+		return keep_b
+	
+	# If one is contaminated keep the other one
+	# If both are keep a
+	if (a.owner.contaminated):
+		return keep_b
+	if (b.owner.contaminated):
+		return keep_a
+	
+	# If one is the WhereBear keep the other
+	# If both are keep a
+	if (b.owner == self):
+		return keep_a
+	if (a.owner == self):
+		return keep_b
+		
+	return keep_a
 
 func catch() -> void:
 	Events.emit_signal("wherebear_caught")
