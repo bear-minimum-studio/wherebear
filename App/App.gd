8c47a56c608885b56f93c1a3aa46c2770990e665
Zelv <contact@elv-studio.com>
2022-07-25 01:57:01 +0200

    add config file that stores and loads fullscreen status

--- App/App.gd
+++ App/App.gd
@@ -1,13 +1,33 @@
 extends Control
 
-var fullscreen := false
+var config = ConfigFile.new()
+var configPath = "user://options.cfg"
+
+var _fullscreen := false
 
 func _ready():
-	pass
+	_load_options()
+
+func _save_options() -> void:
+	config.save(configPath)
+
+func _load_options() -> void:
+	var err = config.load(configPath)
+	if err == OK:
+		_fullscreen = config.get_value("Options", "fullscreen")
+	set_fullscreen(_fullscreen)
+
+func set_fullscreen(fullscreen: bool) -> void:
+	_fullscreen = fullscreen
+	OS.set_window_fullscreen(fullscreen)
+	config.set_value("Options", "fullscreen", _fullscreen)
+	_save_options()
+
+func get_fullscreen() -> bool:
+	return _fullscreen
 
 func _toggle_fullscreen() -> void:
-	fullscreen = not fullscreen
-	OS.set_window_fullscreen(fullscreen)
+	set_fullscreen(not get_fullscreen())
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("ui_fullscreen"):
