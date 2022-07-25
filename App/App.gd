5b803a657ae9b8ad19e9cb9d432b6faf3a76eec3
Zelv <contact@elv-studio.com>
2022-07-25 23:59:13 +0200

    rename options into settings

--- App/App.gd
+++ App/App.gd
@@ -1,45 +1,39 @@
 extends Control
 
 var config = ConfigFile.new()
-var configPath = "user://options.cfg"
+var configPath = "user://settings.cfg"
 
 var _fullscreen := false
 
 # default values are stored in default_bus_layout
-var _master_gain : float
-var _music_gain : float
-var _sfx_gain : float
+var _master_gain := AudioServer.get_bus_volume_db(0)
+var _music_gain := AudioServer.get_bus_volume_db(1)
+var _sfx_gain := AudioServer.get_bus_volume_db(2)
 
 func _ready():
-	_load_options()
+	_load_settings()
 
-func _save_options() -> void:
+func _save_settings() -> void:
 	config.save(configPath)
 
-func _load_options() -> void:
+func _load_settings() -> void:
 	var err = config.load(configPath)
 	if err == OK:
-		var r = config.get_value("Options", "fullscreen")
+		var r = config.get_value("Settings", "fullscreen")
 		if r != null:
 			_fullscreen = r
 
-		r = config.get_value("Options", "master_gain")
+		r = config.get_value("Settings", "master_gain")
 		if r != null:
 			_master_gain = r
-		else:
-			_master_gain = get_master_gain()
 
-		r = config.get_value("Options", "music_gain")
+		r = config.get_value("Settings", "music_gain")
 		if r != null:
 			_music_gain = r
-		else:
-			_music_gain = get_music_gain()
 
-		r = config.get_value("Options", "sfx_gain")
+		r = config.get_value("Settings", "sfx_gain")
 		if r != null:
 			_sfx_gain = r
-		else:
-			_sfx_gain = get_sfx_gain()
 
 	set_fullscreen(_fullscreen)
 	set_master_gain(_master_gain)
@@ -49,8 +43,8 @@ func _load_options() -> void:
 func set_fullscreen(fullscreen: bool) -> void:
 	_fullscreen = fullscreen
 	OS.set_window_fullscreen(fullscreen)
-	config.set_value("Options", "fullscreen", _fullscreen)
-	_save_options()
+	config.set_value("Settings", "fullscreen", _fullscreen)
+	_save_settings()
 
 func get_fullscreen() -> bool:
 	return _fullscreen
@@ -60,24 +54,24 @@ func _toggle_fullscreen() -> void:
 
 func set_master_gain(gain: float) -> void:
 	AudioServer.set_bus_volume_db(0,gain)
-	config.set_value("Options", "master_gain", get_master_gain())
-	_save_options()
+	config.set_value("Settings", "master_gain", get_master_gain())
+	_save_settings()
 
 func get_master_gain() -> float:
 	return AudioServer.get_bus_volume_db(0)
 
 func set_music_gain(gain: float) -> void:
 	AudioServer.set_bus_volume_db(1,gain)
-	config.set_value("Options", "music_gain", get_music_gain())
-	_save_options()
+	config.set_value("Settings", "music_gain", get_music_gain())
+	_save_settings()
 
 func get_music_gain() -> float:
 	return AudioServer.get_bus_volume_db(1)
 
 func set_sfx_gain(gain: float) -> void:
 	AudioServer.set_bus_volume_db(2,gain)
-	config.set_value("Options", "sfx_gain", get_sfx_gain())
-	_save_options()
+	config.set_value("Settings", "sfx_gain", get_sfx_gain())
+	_save_settings()
 
 func get_sfx_gain() -> float:
 	return AudioServer.get_bus_volume_db(2)
